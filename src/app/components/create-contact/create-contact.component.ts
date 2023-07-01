f3700dacf1b0d734861396f94a2117b1268a1844
DeFreitas-Samuel <52897285+DeFreitas-Samuel@users.noreply.github.com>
2023-07-01 16:04:33 -0400

    added a form array to the form component to be able to register a dynamic amount of contact numbers

--- src/app/components/create-contact/create-contact.component.ts
+++ src/app/components/create-contact/create-contact.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
+import { Validators } from '@angular/forms';
+import { FormArray } from '@angular/forms';
 
 
 @Component({
@@ -9,9 +11,11 @@ import { FormBuilder } from '@angular/forms';
 })
 export class CreateContactComponent {
   public contactForm = this.fb.group({
-    firstName: [''],
-    lastName: [''],
-    contactNumber: [''],
+    firstName: ['', Validators.required],
+    lastName: ['', Validators.required],
+    contactNumbers: this.fb.array([
+      this.fb.control('', Validators.required)
+    ]),
   });
 
 
@@ -22,5 +26,21 @@ export class CreateContactComponent {
 
   public onCreateContact() {
     console.log(this.contactForm.value);
+  }
+
+  public get contactNumbers() {
+    return this.contactForm.get('contactNumbers') as FormArray;
+  }
+
+  public onAddNewContactNumber() {
+    this.contactNumbers.push(this.fb.control('', Validators.required))
+  }
+
+  public onRemoveLastContactNumber() {
+    const lastIndexOfContactNumbers = this.contactNumbers.length - 1;
+    if (lastIndexOfContactNumbers > 0) {
+
+      this.contactNumbers.removeAt(lastIndexOfContactNumbers);
+    }
   }
 }
